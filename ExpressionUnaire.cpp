a13940cf536cb27bfb01c87cfb34351fbfb1f049
Lou <lou.hammel@gmail.com>
2017-03-20 15:55:48 +0100

    Getters and Setters for Expression classes

--- ExpressionUnaire.cpp
+++ ExpressionUnaire.cpp
@@ -9,3 +9,33 @@ ExpressionUnaire::~ExpressionUnaire()
 {
 
 }
+
+void setExpression1(Expression &expression)
+{
+	expression1 = expression;
+}
+
+void setSymbole(int &symbole)
+{
+	symbole = symbole;
+}
+
+void setPrefixe(int &prefixe)
+{
+	prefixe = prefixe;
+}
+
+Expression getExpression1()
+{
+	return expression1;
+}
+
+int getSymbole()
+{
+	return symbole;
+}
+
+int getPrefixe()
+{
+	return prefixe;
+}
--- ExpressionUnaire.h
+++ ExpressionUnaire.h
@@ -8,9 +8,17 @@ class ExpressionUnaire : public Expression {
 public:
     ExpressionUnaire();
     ~ExpressionUnaire();
+    void setExpression1(Expression &expression);
+    void setSymbole(int &symbole);
+    void setPrefixe(int &prefixe);
+    Expression getExpression1();
+    int getSymbole();
+    int getPrefixe();
 
 private:
-
+	Expression expression1;
+	int symbole;
+	int prefixe;
 };
 
 #endif //PLD_COMP_EXPRESSIONUNAIRE_H
