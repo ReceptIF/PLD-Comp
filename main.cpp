1c9bb314d0f3b535d6fbed972631c4e4c23171e2
Théo THIBAULT <tthibault@localhost.localdomain>
2017-03-28 10:10:07 +0200

    Add call to generate ass

--- main.cpp
+++ main.cpp
@@ -12,14 +12,20 @@ extern int yydebug;
 int main(void) {
    //yydebug = 1;
    Programme* prog = new Programme();
+   CFG *ir = new CFG();
 
    std::cout << "Etude lexicale du programme" << std::endl;
    yyparse(prog);
    
    std::cout << "Etude de la portée des variables" << std::endl;
    prog->resoudrePortees();
-   
    std::cout << prog->toString() << std::endl;
+   
+   std::cout << "Generation de l'IR" << std::endl;
+   ir->mettreEnPlaceIR(prog);
+   
+   std::cout << "Generation de l'assembleur" << std::endl;
+   std::cout << ir->genererAssembleur(); << std::endl;
 
    delete prog;
 
